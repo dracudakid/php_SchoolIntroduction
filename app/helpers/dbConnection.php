afb2366fe09b9e1e9b4c4df69d2c85ac7805951b
dracudakid <tandat2209@gmail.com>
2016-05-07 21:12:21 +0700

    update db & dbconnection.php

---
+++ app/helpers/dbConnection.php
@@ -0,0 +1,22 @@
+<?php
+   
+ class MysqliConnection{
+   private static $dbhost = 'localhost';
+   private static $dbuser = 'root';
+   private static $dbpass = '';
+   private static $dbname = 'school';
+   private static $connection = null;
+   public static function getConnection(){
+       if(self::$connection === null){
+           //Here get some routine to make your configuration extern
+           self::$connection = new Mysqli(self::$dbhost, self::$dbuser, self::$dbpass, self::$dbname);
+           if(mysqli_connect_errno()){
+				echo "Ket noi loi db:".mysqli_connect_error()."<br />";
+			}
+       }
+       return self::$connection;
+}
+}
+	$connection = MysqliConnection::getConnection();
+	
+?>
